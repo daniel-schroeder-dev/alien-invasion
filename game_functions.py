0ce94ec1d71228d105c5d02845adb0e4f85edec8
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-29 09:58:19 -0500

    Refactor check_events to use helper functions to handle key(down/up) events.

--- game_functions.py
+++ game_functions.py
@@ -8,15 +8,23 @@ def check_events(ship):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
+
+def check_keydown_events(event, ship):
+    """Handle keydown events."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    """Handle keyup events."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
 
 def update_screen(ai_settings, screen, ship):
     """Update images on the screen and flip to the new screen."""
@@ -25,4 +33,3 @@ def update_screen(ai_settings, screen, ship):
 
     # Make the most recently drawn screen visible.
     pygame.display.flip()
-    
